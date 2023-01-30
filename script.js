f9daf6fb13ece0d0692a390f404d3399eaac88d0
seansg <seansgarwood@gmail.com>
2023-01-30 12:45:20 -0500

    Define operate fn

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case 'add':
+      return add(a, b);
+      break;
+    case 'subtract':
+      return subtract(a, b);
+      break;
+    case 'multiply':
+      return multiply(a, b);
+      break;
+    case 'divide':
+      return divide(a, b);
+  }
+}
